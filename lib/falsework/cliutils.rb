1163bd75a8fea4984b33e1ce70ffbd336bc391cc
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2012-03-05 04:54:33 +0200

    Added preferable exit codes into cliutils.

--- lib/falsework/cliutils.rb
+++ lib/falsework/cliutils.rb
@@ -7,6 +7,24 @@ require 'pathname'
 require_relative 'meta'
 
 module Falsework
+
+  # Preferable exit codes. See sysexits(3) in FreeBSD.
+  EX_OK = 0
+  EX_USAGE = 64
+  EX_DATAERR = 65
+  EX_NOINPUT = 66
+  EX_NOUSER = 67
+  EX_NOHOST = 68
+  EX_UNAVAILABLE = 69
+  EX_SOFTWARE = 70
+  EX_OSERR = 71
+  EX_OSFILE = 72
+  EX_CANTCREAT = 73
+  EX_IOERR = 74
+  EX_TEMPFAIL = 75
+  EX_PROTOCOL = 76
+  EX_NOPERM = 77
+  EX_CONFIG = 78
   
   # Common routines useful in any CLI program.
   class CliUtils
