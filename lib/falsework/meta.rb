6bde8b966b81d131d61f14ed6db5513c9d0aabe2
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2013-04-07 22:15:50 +0300

    update version number & NEWS file

--- lib/falsework/meta.rb
+++ lib/falsework/meta.rb
@@ -2,7 +2,7 @@
 module Falsework
   module Meta # :nodoc:
     NAME = 'falsework'
-    VERSION = '3.1.1.alpha'
+    VERSION = '3.2.0'
     AUTHOR = 'Alexander Gromnitsky'
     EMAIL = 'alexander.gromnitsky@gmail.com'
     HOMEPAGE = 'http://github.com/gromnitsky/' + NAME
